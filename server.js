fb14e2850e257b60a5a5f1fa4d112264e6c14668
Allan López <allanloji@gmail.com>
2017-07-29 13:11:20 -0500

    Update language

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ var middleware = {
 
 app.use(middleware.logger);
 
-//about
 //about us
 app.get('/about', middleware.requireAuthentication,function (req, res) {
-  res.send('About Us');
+  res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
